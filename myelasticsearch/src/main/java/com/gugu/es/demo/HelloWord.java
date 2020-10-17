0971c6be81b00ff851d67fca02ece654c2a03961
gugu <gu1230gu1230@gmail.com>
2020-10-17 12:20:31 +0800

    es 添加CURD

--- myelasticsearch/src/main/java/com/gugu/es/demo/HelloWord.java
+++ myelasticsearch/src/main/java/com/gugu/es/demo/HelloWord.java
@@ -9,7 +9,6 @@ import org.elasticsearch.common.transport.TransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
 
 /**
  * @author gugu
@@ -19,27 +18,29 @@ import java.net.UnknownHostException;
  */
 public class HelloWord {
 
-    public static final  String HOST_NAME = "localhost";
-
-    public static void main(String[] args) throws UnknownHostException {
+    public static final  String HOST_NAME0 = "192.168.2.100";
+    public static final  String HOST_NAME1 = "192.168.2.101";
+    public static final  String HOST_NAME2 = "192.168.2.102";
+    public static final int port = 9300;
+    public static void main(String[] args) throws Exception {
         // 设置集群名字
         Settings settings = Settings.builder()
-                .put("cluster.name", "gugu-es")
+                .put("cluster.name", "my-es")
                 .build();
         //创建client old 将在8.0废弃
         TransportClient client = new PreBuiltTransportClient(settings).addTransportAddresses(
                 //用java访问ES用的端口是9300
-                new TransportAddress(InetAddress.getByName(HOST_NAME), 9300),
-                new TransportAddress(InetAddress.getByName(HOST_NAME), 9301),
-                new TransportAddress(InetAddress.getByName(HOST_NAME), 9302)
+                new TransportAddress(InetAddress.getByName(HOST_NAME0), port),
+                new TransportAddress(InetAddress.getByName(HOST_NAME1), port),
+                new TransportAddress(InetAddress.getByName(HOST_NAME2), port)
         );
 //        RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(
-//                new HttpHost("localhost", 9200, "http"),
-//                new HttpHost("localhost", 9201, "http"),
-//                new HttpHost("localhost", 9202, "http")
+//                new HttpHost(HOST_NAME0, 9200, "http"),
+//                new HttpHost(HOST_NAME1, 9201, "http"),
+//                new HttpHost(HOST_NAME2, 9202, "http")
 //        ));
 
-        GetResponse documentFields = client.prepareGet("index","_doc","1").execute().actionGet();
+        GetResponse documentFields = client.prepareGet("kibana_sample_data_flights","_doc","SutcInUBQRrfBaQfiaye").execute().actionGet();
         System.out.println(documentFields);
 
         client.close();
--- mylucene/src/test/java/com/gugu/lucene/HelloWorldTest.java
+++ mylucene/src/test/java/com/gugu/lucene/HelloWorldTest.java
@@ -1,6 +1,5 @@
 package com.gugu.lucene;
 
-import com.sun.xml.internal.txw2.output.IndentingXMLFilter;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
