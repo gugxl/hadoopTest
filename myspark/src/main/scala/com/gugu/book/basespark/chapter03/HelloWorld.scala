be99cf008459ec217d2530c732c1a2e1ac93aa57
gugu <gu1230gu1230@gmail.com>
2023-09-16 11:40:02 +0800

    spark设计与运行原理

---
+++ myspark/src/main/scala/com/gugu/book/basespark/chapter03/HelloWorld.scala
@@ -0,0 +1,15 @@
+package com.gugu.book.basespark.chapter03
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.{SparkConf, SparkContext}
+
+object HelloWorld {
+    def main(args: Array[String]): Unit = {
+        val conf = new SparkConf().setAppName("hello wrld").setMaster("local[*]")
+        val sc = new SparkContext(conf)
+        val fileRDD: RDD[String] = sc.textFile("/data/tmp/wc/input")
+        val l: Long = fileRDD.filter(_.contains("a")).count()
+        println(l)
+    }
+    
+}
